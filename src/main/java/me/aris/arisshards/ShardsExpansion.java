d947937246aad248c8d072e5be28a570fc98a440
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 11:48:24 +0700

    Create ShardsExpansion.java

---
+++ src/main/java/me/aris/arisshards/ShardsExpansion.java
@@ -0,0 +1,30 @@
+package me.aris.arisshards;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+
+public class ShardsExpansion extends PlaceholderExpansion {
+    private final ArisShards plugin;
+    public ShardsExpansion(ArisShards plugin) { this.plugin = plugin; }
+    @Override
+    public String getIdentifier() { return "arisshards"; }
+    @Override
+    public String getAuthor() { return "VennLMAO"; }
+    @Override
+    public String getVersion() { return "1.0"; }
+    @Override
+    public boolean persist() { return true; }
+    @Override
+    public String onPlaceholderRequest(Player player, String params) {
+        if (player == null) return "0";
+        if (params.equalsIgnoreCase("count")) {
+            return format(plugin.getDataManager().getShards(player.getUniqueId()));
+        }
+        return null;
+    }
+    private String format(int n) {
+        if (n < 1000) return String.valueOf(n);
+        if (n < 1000000) return (n / 1000) + plugin.getConfig().getString("amount-format.k");
+        return (n / 1000000) + plugin.getConfig().getString("amount-format.m");
+    }
+          }
