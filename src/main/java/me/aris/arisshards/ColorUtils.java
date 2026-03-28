f86f56137487960b03126c0865b58226220a35b4
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 13:04:54 +0700

    Update ColorUtils.java

--- src/main/java/me/aris/arisshards/ColorUtils.java
+++ src/main/java/me/aris/arisshards/ColorUtils.java
@@ -8,11 +8,14 @@ public class ColorUtils {
     private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
 
     public static String color(String message) {
+        if (message == null) return "";
         Matcher matcher = HEX_PATTERN.matcher(message);
         StringBuffer buffer = new StringBuffer();
         while (matcher.find()) {
-            matcher.appendReplacement(buffer, ChatColor.of("#" + matcher.group(1)).toString());
+            String hexCode = matcher.group(1);
+            ChatColor color = ChatColor.of("#" + hexCode);
+            matcher.appendReplacement(buffer, color.toString());
         }
         return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
     }
-          }
+}
