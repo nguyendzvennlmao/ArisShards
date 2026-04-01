ada33b64768d3d036eed18fdcd5a24fc1eca5940
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 08:01:06 +0700

    Update ShardsExpansion.java

--- src/main/java/me/aris/arisshards/ShardsExpansion.java
+++ src/main/java/me/aris/arisshards/ShardsExpansion.java
@@ -5,26 +5,35 @@ import org.bukkit.entity.Player;
 
 public class ShardsExpansion extends PlaceholderExpansion {
     private final ArisShards plugin;
+
     public ShardsExpansion(ArisShards plugin) { this.plugin = plugin; }
+
     @Override
     public String getIdentifier() { return "arisshards"; }
+
     @Override
     public String getAuthor() { return "VennLMAO"; }
+
     @Override
     public String getVersion() { return "1.0"; }
+
     @Override
     public boolean persist() { return true; }
+
     @Override
     public String onPlaceholderRequest(Player player, String params) {
         if (player == null) return "0";
-        if (params.equalsIgnoreCase("count")) {
+        if (params.equalsIgnoreCase("balance")) {
             return format(plugin.getDataManager().getShards(player.getUniqueId()));
         }
         return null;
     }
+
     private String format(int n) {
         if (n < 1000) return String.valueOf(n);
-        if (n < 1000000) return (n / 1000) + plugin.getConfig().getString("amount-format.k");
-        return (n / 1000000) + plugin.getConfig().getString("amount-format.m");
+        String k = plugin.getConfig().getString("amount-format.k", "K");
+        String m = plugin.getConfig().getString("amount-format.m", "M");
+        if (n < 1000000) return (n / 1000) + k;
+        return (n / 1000000) + m;
     }
-          }
+}
